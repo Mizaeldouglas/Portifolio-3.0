027ce8d3b0645f2997f41c676b4e97dda37e7050
Mizael Douglas <mizaeldouglas2@gmail.com>
2023-03-19 13:51:39 -0300

    first commit

---
+++ src/Components/Projects/Projects.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import ProjectsPage from 'pages/ProjectsPage';
+import Project from './Project/Project';
+import styles from './Projects.module.scss';
+
+export default function Projects({ projects }) {
+    const { title, projects: portfolio, button, } = projects;
+
+    const renderProjects = portfolio.map((project) => (
+        <Project key={project._key} project={project} />
+    ));
+    return (
+        <section className={styles.projects} id='projects' data-aos="fade-up">
+            <h2>{title}</h2>
+            <ul className={styles.list}>{renderProjects}</ul>
+            <Link href="/ProjectsPage" as="/projetos" project={projects} className='btn btn-primary'>
+                {button}
+            </Link>
+        </section>
+    )
+}
